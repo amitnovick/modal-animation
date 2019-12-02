c0d4f8fdbaaa228dea040ac19af8488f315bf4ab
Amit Novick <axnovick@gmail.com>
2019-12-02 15:47:06 +0200

    Tweaked modal

--- src/ItemModal/ItemModal.js
+++ src/ItemModal/ItemModal.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Modal from "react-modal";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./styles.module.scss";
 
@@ -25,8 +27,15 @@ const ItemModal = React.memo(
         }}
       >
         <div className={styles["modal-top-bar"]}>
+          <button className={styles["left-arrow-button"]} onClick={closeModal}>
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+          <button className={styles["back-button"]} onClick={closeModal}>
+            Back
+          </button>
+
           {item !== null ? (
-            <div className={styles["artist-bar"]}>
+            <div className={styles["artist-bar-large-screen"]}>
               <img
                 className={styles["artist-image"]}
                 src={item.artistImage}
@@ -39,10 +48,22 @@ const ItemModal = React.memo(
             </div>
           ) : null}
           <button className={styles["close-button"]} onClick={closeModal}>
-            ×
+            <FontAwesomeIcon icon={faTimes} />
           </button>
         </div>
-
+        {item !== null ? (
+          <div className={styles["artist-bar-small-screen"]}>
+            <img
+              className={styles["artist-image"]}
+              src={item.artistImage}
+              alt="arist-avatar"
+            />
+            <div className={styles["artist-identity-container"]}>
+              <span className={styles["artist-name"]}>{item.artistName}</span>
+              <span className={styles["artist-id"]}>{item.artistId}</span>
+            </div>
+          </div>
+        ) : null}
         <div className={styles["image-container"]}>
           <img
             ref={modalImageRef}
