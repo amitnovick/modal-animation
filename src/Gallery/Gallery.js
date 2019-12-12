edff64c2efae1bd1d51d9c7c4d79b30f266386e9
Amit Novick <axnovick@gmail.com>
2019-12-12 22:27:17 +0200

    Added basic route management and refactored

---
+++ src/Gallery/Gallery.js
@@ -0,0 +1,412 @@
+import React from "react";
+import "normalize.css";
+import { useMachine } from "@xstate/react";
+
+import "./index.scss";
+import Modal from "./Modal/Modal";
+import machine from "./machine";
+import usePortal from "../utils/usePortal";
+import usePrevious from "../utils/usePrevious";
+
+const initialItems = {
+  FvVO3o6Gw8g: {
+    imageUrl:
+      "https://images.unsplash.com/photo-1574797914072-46cc7da9b5aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    imageDescription: "brown grass photo",
+    artistName: "Jessica Fadel",
+    artistId: "@jessicalfadel",
+    artistImage:
+      "https://images.unsplash.com/photo-1574797914072-46cc7da9b5aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+  },
+  sVqrUN9RZIo: {
+    imageUrl:
+      "https://images.unsplash.com/photo-1488810050923-3897e8aa9037?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    imageDescription: "snow covered brown and gray mountain photo",
+    artistName: "MR WONG",
+    artistId: "@mrwong",
+    artistImage:
+      "https://images.unsplash.com/profile-1493107212406-6bb991c88c50?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff"
+  },
+  "1AwGus3QVE4": {
+    imageUrl:
+      "https://images.unsplash.com/photo-1560331470-74c11f1c2ccc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+
+    imageDescription: "selective focus photography of green grass photo",
+    artistName: "Tisma Jrdl",
+    artistId: "@tisma",
+    artistImage:
+      "https://images.unsplash.com/profile-fb-1487182403-063ac21af7d6.jpg?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff"
+  },
+  NDuPLKYRXQU: {
+    imageUrl:
+      "https://images.unsplash.com/photo-1431512284068-4c4002298068?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80",
+    imageDescription: "bird's-eye view of mountain rang photo",
+    artistName: "Elena Prokofyeva",
+    artistId: "@leni_eleni",
+    artistImage:
+      "https://images.unsplash.com/profile-1474569303338-12538796d50b?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff"
+  },
+  sMQiL_2v4vs: {
+    imageUrl:
+      "https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+
+    imageDescription: "large tree in middle of forest during daytime photo",
+    artistName: "veeterzy",
+    artistId: "@veeterzy",
+    artistImage:
+      "https://images.unsplash.com/profile-1506670759095-e8bab484cbc5?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff"
+  },
+  "01_igFr7hd4": {
+    imageUrl:
+      "https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=840&q=80",
+    imageDescription: "bird's eye photograph of green mountains photo",
+    artistName: "Qingbao Meng",
+    artistId: "@ideasboom",
+    artistImage:
+      "https://images.unsplash.com/profile-1536207867484-ee3f39fa0ee9?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff"
+  },
+  pQMM63GE7fo: {
+    imageUrl:
+      "https://images.unsplash.com/photo-1442850473887-0fb77cd0b337?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    imageDescription:
+      "landscape photo of waterfalls flowing into river during daytime photo",
+    artistName: "Milada Vigerova",
+    artistId: "@mili_vigerova",
+    artistImage:
+      "https://images.unsplash.com/profile-1478097671033-99cc86c22f38?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff"
+  },
+  "3Eqc3Ph4oRg": {
+    imageUrl:
+      "https://images.unsplash.com/photo-1529832393073-e362750f78b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+
+    imageDescription: "photo of clouds photo",
+    artistName: "Dominik Dancs",
+    artistId: "@dodancs",
+    artistImage:
+      "https://images.unsplash.com/profile-1547587995842-12c50c01581e?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff"
+  },
+  nKNm_75lH4g: {
+    imageUrl:
+      "https://images.unsplash.com/photo-1522684487319-f574cd5937cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+
+    imageDescription: "landscape photo of sand photo",
+    artistName: "Hasan Almasi",
+    artistId: "@hasanalmasi",
+    artistImage:
+      "https://images.unsplash.com/profile-1574743414193-d9280295ec96image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff"
+  }
+};
+
+function preloadImage(url) {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.src = url;
+    image.onload = () => resolve(image);
+  });
+}
+
+const fetchAndPreloadImages = async items => {
+  const preloadedImages = await Promise.all(
+    Object.entries(items).map(([itemId, { imageUrl }]) =>
+      preloadImage(imageUrl).then(image => [itemId, image])
+    )
+  );
+
+  return preloadedImages;
+};
+
+const normalizeImagesIntoItems = (preloadedImages, items) => {
+  const imagesByItemId = preloadedImages.reduce(
+    (accumulated, [itemId, image]) => {
+      return {
+        ...accumulated,
+        [itemId]: image
+      };
+    },
+    {}
+  );
+
+  const itemsWithImages = Object.entries(items).reduce(
+    (accumulated, [itemId, itemData]) => {
+      return {
+        ...accumulated,
+        [itemId]: {
+          ...itemData,
+          image: imagesByItemId[itemId]
+        }
+      };
+    },
+    {}
+  );
+
+  return itemsWithImages;
+};
+
+const applyStyles = (element, stylesOptions) => {
+  Object.assign(element.style, stylesOptions);
+};
+
+const performLastInvertPlay = ({ element, last, first }) => {
+  const deltaX = first.left - last.left;
+  const deltaY = first.top - last.top;
+  const deltaW = last.width === 0 ? 0 : first.width / last.width; // working around a bug that occurs when switching between responsive layout and normal layout in devtools
+  const deltaH = last.height === 0 ? 0 : first.height / last.height; // working around a bug that occurs when switching between responsive layout and normal layout in devtools
+
+  const centerFirstX = first.left + first.width / 2;
+  const centerFirstY = first.top + first.height / 2;
+
+  const centerLastX = last.left + last.width / 2;
+  const centerLastY = last.top + last.height / 2;
+
+  const distance = Math.sqrt(
+    Math.pow(centerFirstX - centerLastX, 2) +
+      Math.pow(centerFirstY - centerLastY, 2)
+  );
+
+  const duration = distance < 200 ? 300 : 400; // If the grid item is really close, make duration short
+
+  const animation = element.animate(
+    [
+      {
+        transformOrigin: "top left",
+        transform: `
+        translate(${deltaX}px, ${deltaY}px)
+        scale(${deltaW}, ${deltaH})
+      `
+      },
+      { transformOrigin: "top left", transform: "none" }
+    ],
+    {
+      // timing options
+      duration: duration,
+      easing: "ease-in-out",
+      fill: "both"
+    }
+  );
+
+  return animation;
+};
+
+const Gallery = () => {
+  const [extendedState, setExtendedState] = React.useState({
+    items: initialItems,
+    chosenItemId: null,
+    hasFinishedLoading: false
+  });
+
+  const { items, chosenItemId, hasFinishedLoading } = extendedState;
+
+  const TransitionElementPortal = usePortal();
+  const ModalPortal = usePortal();
+
+  const portalImageRef = React.useRef();
+
+  const animationRef = React.useRef();
+
+  const gridImagesRef = React.useRef(
+    Object.keys(extendedState.items).reduce(
+      (accumulated, itemId) => ({
+        ...accumulated,
+        [itemId]: React.createRef()
+      }),
+      {}
+    )
+  );
+
+  const modalImageRef = React.useRef();
+
+  const [state, send] = useMachine(machine, {
+    devTools: true
+  });
+
+  const previousState = usePrevious(state);
+
+  React.useLayoutEffect(() => {
+    if (previousState) {
+      if (state.matches("closed->opened") && previousState.matches("closed")) {
+        const last = modalImageRef.current.getBoundingClientRect();
+        applyStyles(portalImageRef.current, {
+          top: last.top + "px",
+          left: last.left + "px",
+          width: last.width + "px",
+          height: last.height + "px"
+        });
+        const animation = performLastInvertPlay({
+          element: portalImageRef.current,
+          first: gridImagesRef.current[
+            chosenItemId
+          ].current.getBoundingClientRect(),
+          last: last
+        });
+        animation.onfinish = () => send("FINISHED_SLIDE_IN_ANIMATION");
+        animationRef.current = animation;
+      } else if (
+        state.matches("opened->closed") &&
+        previousState.matches("closed->opened")
+      ) {
+        const gridImageRect = gridImagesRef.current[
+          chosenItemId
+        ].current.getBoundingClientRect();
+        const portalImageRect = portalImageRef.current.getBoundingClientRect();
+        animationRef.current.cancel();
+        applyStyles(portalImageRef.current, {
+          top: gridImageRect.top + "px",
+          left: gridImageRect.left + "px",
+          width: gridImageRect.width + "px",
+          height: gridImageRect.height + "px"
+        });
+        const animation = performLastInvertPlay({
+          element: portalImageRef.current,
+          first: portalImageRect,
+          last: gridImageRect
+        });
+        animation.onfinish = () => send("FINISHED_SLIDE_OUT_ANIMATION");
+      } else if (
+        state.matches("opened->closed") &&
+        previousState.matches("opened")
+      ) {
+        const gridImageRect = gridImagesRef.current[
+          chosenItemId
+        ].current.getBoundingClientRect();
+        applyStyles(portalImageRef.current, {
+          top: gridImageRect.top + "px",
+          left: gridImageRect.left + "px",
+          width: gridImageRect.width + "px",
+          height: gridImageRect.height + "px"
+        });
+        const modalImageRect = modalImageRef.current.getBoundingClientRect();
+        const animation = performLastInvertPlay({
+          element: portalImageRef.current,
+          first: modalImageRect,
+          last: gridImageRect
+        });
+        animation.onfinish = () => send("FINISHED_SLIDE_OUT_ANIMATION");
+      }
+    }
+  }, [state.value]);
+
+  const updateItems = async () => {
+    const preloadedImages = await fetchAndPreloadImages(extendedState.items);
+    const itemsWithImages = normalizeImagesIntoItems(
+      preloadedImages,
+      extendedState.items
+    );
+    setExtendedState(prev => ({
+      ...prev,
+      items: itemsWithImages,
+      hasFinishedLoading: true
+    }));
+  };
+
+  React.useEffect(() => {
+    updateItems();
+  }, []);
+
+  React.useEffect(() => {
+    const listener = ({ key }) => {
+      if (key === "Escape") {
+        send("CLOSE_MODAL");
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => window.removeEventListener("keydown", listener);
+  }, []);
+
+  const isOpeningModal = state.matches("closed->opened");
+
+  React.useEffect(() => {
+    if (isOpeningModal) {
+      const listener = ({ target }) => {
+        if (!portalImageRef.current.contains(target)) {
+          send("CLOSE_MODAL");
+        }
+      };
+
+      window.addEventListener("mousedown", listener);
+
+      return () => window.removeEventListener("mousedown", listener);
+    }
+  }, [isOpeningModal]);
+
+  console.log("*** <App RENDER> ***");
+  console.log("extendedState:", extendedState);
+  console.log("state.value:", state.value);
+  console.log(
+    "previous state.value:",
+    previousState ? previousState.value : undefined
+  );
+  console.log("*** </App RENDER> ***");
+
+  const shouldDisplayPortalImage =
+    state.matches("closed->opened") || state.matches("opened->closed");
+
+  if (!hasFinishedLoading) {
+    return <h2> Loading... </h2>;
+  } else {
+    return (
+      <>
+        <div className="grid">
+          {Object.entries(items).map(
+            ([
+              itemId,
+              {
+                image: { src: imageSrc },
+                imageDescription
+              }
+            ]) => (
+              <div key={itemId} className="image-container">
+                <img
+                  {...{
+                    style:
+                      (shouldDisplayPortalImage || state.matches("opened")) &&
+                      chosenItemId === itemId
+                        ? {
+                            visibility: "hidden"
+                          }
+                        : {}
+                  }}
+                  key={itemId}
+                  ref={gridImagesRef.current[itemId]}
+                  className="image"
+                  src={imageSrc}
+                  alt={imageDescription}
+                  onClick={() => {
+                    if (state.matches("closed")) {
+                      send("OPEN_MODAL");
+                      setExtendedState(previous => ({
+                        ...previous,
+                        chosenItemId: itemId
+                      }));
+                    }
+                  }}
+                />
+              </div>
+            )
+          )}
+        </div>
+        <ModalPortal>
+          <Modal
+            item={state.matches("closed") ? null : items[chosenItemId]}
+            modalState={state}
+            closeModal={() => send("CLOSE_MODAL")}
+            modalImageRef={modalImageRef}
+          />
+        </ModalPortal>
+        <TransitionElementPortal>
+          {shouldDisplayPortalImage ? (
+            <img
+              className="image"
+              ref={portalImageRef}
+              src={items[chosenItemId].image.src}
+              style={{ position: "fixed" }}
+              alt=""
+            />
+          ) : null}
+        </TransitionElementPortal>
+      </>
+    );
+  }
+};
+
+export default Gallery;
